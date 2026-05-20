143c89dbc716a455fdec334cbf7ee27f7f0d6f9a
AliaDonley <alia.donley@usu.edu>
2026-05-20 11:57:31 -0600

    Create SubsubsetFasta.sh

---
+++ SubsubsetFasta.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=96:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=12
+#SBATCH --account=usubio-kp
+#SBATCH --partition=usubio-kp
+#SBATCH --job-name=subsetfasta
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=alia.donley@usu.edu
+
+cd /uufs/chpc.utah.edu/common/home/u6047808/LycLotis/ZLycLotis
+
+ml perl
+
+perl SubSetFasta.pl
