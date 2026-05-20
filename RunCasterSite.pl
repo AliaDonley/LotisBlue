3ad6dadcf1b156aeec9af620fccac614ac1a0ec4
AliaDonley <alia.donley@usu.edu>
2026-05-20 12:04:34 -0600

    Create RunCasterSite.pl

---
+++ RunCasterSite.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+#
+
+foreach $i (1..23){
+
+        system "/uufs/chpc.utah.edu/common/home/u6047808/bin/ASTER-Linux/bin/caster-site -i sub_CASTchromad1_fff_o_lycpool_chrom$i.fasta -o CASTcout_max_$i --root Lyc-MEN12 --thread 24\n";
+#       system "ASTER-Linux/bin/caster-site -i sub_CASTchromad1_fff_o_lycpool_chrom$i.fasta -o CASTcout_max_$i --root MEN --thread 24\n";
+}
